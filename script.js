3a1fe5bc530603af39f01bea7261557ffe179560
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-01-19 00:22:02 +0500

    btn listeners

--- script.js
+++ script.js
@@ -15,17 +15,33 @@ function Gameboard () {
     function renderButtons (row,col) {
         const button = document.createElement('button');
         button.id = `${row}, ${col}`;
+        button.className = 'btn'
         document.getElementById(`${row}`).appendChild(button);
     }
+
+    function renderMark (marker) {
+        document.getElementsByClassName('btn').innerText = `${marker}`
+    }
+
 }
 
 Gameboard()
 
-function players (player1, player2) {
+function GameControls () {
     const playerOne = player1;
     const playerTwo = player2;
-
+    
     function switchPlayerPlayer () {
         activePlayer = activePlayer === player1 ? player2 : player1;
     }
+    
+    const playerMark = x    
+
+    function btnListener (marker) {
+        document.getElementsByClassName("btn").addEventListener("click", function() {  
+            renderMark(marker)  
+        });
+    }
+
+    btnListener(playerMark)
 }
